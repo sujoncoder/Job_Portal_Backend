464f1042fa0395ad15ec5abc464c135a8168e8d7
SaqlineShabbir <saqline5869@gmail.com>
2023-12-22 19:11:37 +0600

    changes

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import app from "./app.js";
 // port assign
 const port = process.env.PORT || 8000;
 
-
+//
 // app listen
 app.listen(port, () => {
     console.log(`Server is running ${port}`.bgBlue.black)
